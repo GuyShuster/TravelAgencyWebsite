cb212083edf7ef435c48aa6173a1597ebab83afa
Guy Shuster <guyshu@post.bgu.ac.il>
2023-01-01 12:28:05 +0200

    basinc express skeleton

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 import express from 'express';
-import process from 'process';
+import config from './config.js';
+import birds from './routes/test-a.js';
+import dogs from './routes/test-b.js';
 
-
-async function main() {
-    
+function addRoutes(app) {
+    app.use('/birds', birds);
+    app.use('/dogs', dogs);
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    });
 }
 
-try {
-    await main();
-} catch (error) {
-    // eslint-disable-next-line
-    console.log(`Error: ${error.message}`);
-} finally {
-    process.exit();
+function startListening(app, port) {
+    app.listen(port, () => {
+        // eslint-disable-next-line
+        console.log(`Travel Agency listening on port ${port}`)
+    });
 }
+
+
+const app = express();
+addRoutes(app);
+startListening(app, config.port);
---
+++ src/config.js
@@ -0,0 +1,5 @@
+import process from 'process';
+
+export default {
+    port: process.env.PORT || 80,
+}
