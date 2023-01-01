77c4085534c759dd7a6106b23575502fa198c599
Guy Shuster <guyshu@post.bgu.ac.il>
2023-01-01 11:23:33 +0200

    complete project skeleton

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 import express from 'express';
+import process from 'process';
 
+
+async function main() {
+    
+}
+
+try {
+    await main();
+} catch (error) {
+    // eslint-disable-next-line
+    console.log(`Error: ${error.message}`);
+} finally {
+    process.exit();
+}
