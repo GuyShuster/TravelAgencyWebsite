003b99e72138e0f7a5643b5380a3f1b4b7ba1360
Guy Shuster <guyshu@post.bgu.ac.il>
2023-01-01 13:15:49 +0200

    add mongoose and prettify app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import process from 'process';
 import config from './config.js';
 import birds from './routes/test-a.js';
 import dogs from './routes/test-b.js';
+
+function initAppObject(app) {
+    app.use(express.json());
+}
 
 function addRoutes(app) {
     app.use('/birds', birds);
@@ -11,14 +17,34 @@ function addRoutes(app) {
     });
 }
 
+async function connectToDB(dbURL) {
+    await mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
 function startListening(app, port) {
-    app.listen(port, () => {
-        // eslint-disable-next-line
-        console.log(`Travel Agency listening on port ${port}`)
+    return new Promise((resolve, reject) => {
+        app.listen(port)
+            .once('listening', resolve)
+            .once('error', reject);
     });
 }
 
+async function main() {
+    const app = express();
 
-const app = express();
-addRoutes(app);
-startListening(app, config.port);
+    initAppObject(app);
+    addRoutes(app);
+    await connectToDB(config.dbURL);
+    await startListening(app, config.port);
+
+    // eslint-disable-next-line
+    console.log(`Travel Agency listening on port ${config.port}!`);
+}
+
+try {
+    await main();
+} catch (error) {
+    // eslint-disable-next-line
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+}
--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ import process from 'process';
 
 export default {
     port: process.env.PORT || 80,
+    dbURL: process.env.DB_URL || 'mongodb://127.0.0.1:27017/travel-agency'
 }
